b3f55b92ce5e9739c49f7f6668e2d25b48c95844
周瑾 <zhoujin@zhoujins-MacBook-Air.local>
2017-06-16 02:34:29 +0800

    step 2

--- imports/api/Server/webapp.js
+++ imports/api/Server/webapp.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import JsSHA from 'jssha';
-// import xml from 'node-xml';
-import xml2js from 'xml2js';
+import xml from 'node-xml';
 
 WebApp.connectHandlers.use('/hello', (req, res) => {
   const test = Meteor.call('addToTest');
@@ -37,73 +36,53 @@ WebApp.connectHandlers.use('/wechat', (req, res) => {
       let CreateTime = '';
       let MsgType = '';
       let Content = '';
-      xml2js.parseString(xmlStr, (err, result) => {
-        console.log(result);
-        const xml = result.xml;
-        ToUserName = xml.ToUserName[0];
-        FromUserName = xml.FromUserName[0];
-        CreateTime = xml.CreateTime[0];
-        MsgType = xml.MsgType[0];
-        if (MsgType === 'text') {
-          Content = xml.Content[0];
-        }
-        const msg = Content;
-        const sendMessage = `
-            <xml>
-              <ToUserName><![CDATA[${FromUserName}]]></ToUserName>
-              <FromUserName><![CDATA[${ToUserName}]]></FromUserName>
-              <CreateTime>${CreateTime}</CreateTime>
-              <MsgType><![CDATA[text]]></MsgType>
-              <Content><![CDATA[${msg}]]></Content>
-            </xml>`;
-        res.end(sendMessage);
-      });
+      let tempName = '';
       // 开始解析消息
-      // const parse = new xml.SaxParser((cb) => {
-      //   cb.onStartElementNS((elem, attra, prefix, uri, namespaces) => {
-      //     tempName = elem;
-      //   });
-      //   cb.onCharacters((chars) => {
-      //     const thischars = chars.replace(/(^\s*)|(\s*$)/g, '');
-      //     if (tempName === 'CreateTime') {
-      //       CreateTime = thischars;
-      //     }
-      //   });
-      //   cb.onCdata((cdata) => {
-      //     if (tempName === 'ToUserName') {
-      //       ToUserName = cdata;
-      //     } else if (tempName === 'FromUserName') {
-      //       FromUserName = cdata;
-      //     } else if (tempName === 'MsgType') {
-      //       MsgType = cdata;
-      //     } else if (tempName === 'Content') {
-      //       Content = cdata;
-      //     }
-      //     console.log(tempName, ':', cdata);
-      //   });
-      //   cb.onEndElementNS((elem, prefix, uri) => {
-      //     tempName = '';
-      //   });
-      //   cb.onEndDocument(() => {
-      //     // 按收到的消息格式回复消息
-      //     CreateTime = parseInt(new Date().getTime() / 1000, 10);
-      //     let msg = '';
-      //     if (MsgType === 'text') {
-      //       msg = Content;
-      //       // 组织返回的数据包
-      //       const sendMessage = `
-      //           <xml>
-      //             <ToUserName><![CDATA[${FromUserName}]]></ToUserName>
-      //             <FromUserName><![CDATA[${ToUserName}]]></FromUserName>
-      //             <CreateTime>${CreateTime}</CreateTime>
-      //             <MsgType><![CDATA[text]]></MsgType>
-      //             <Content><![CDATA[${msg}]]></Content>
-      //           </xml>`;
-      //       res.end(sendMessage);
-      //     }
-      //   });
-      // });
-      // parse.parseString(xmlStr);
+      const parse = new xml.SaxParser((cb) => {
+        cb.onStartElementNS((elem, attra, prefix, uri, namespaces) => {
+          tempName = elem;
+        });
+        cb.onCharacters((chars) => {
+          const thischars = chars.replace(/(^\s*)|(\s*$)/g, '');
+          if (tempName === 'CreateTime') {
+            CreateTime = thischars;
+          }
+        });
+        cb.onCdata((cdata) => {
+          if (tempName === 'ToUserName') {
+            ToUserName = cdata;
+          } else if (tempName === 'FromUserName') {
+            FromUserName = cdata;
+          } else if (tempName === 'MsgType') {
+            MsgType = cdata;
+          } else if (tempName === 'Content') {
+            Content = cdata;
+          }
+          console.log(tempName, ':', cdata);
+        });
+        cb.onEndElementNS((elem, prefix, uri) => {
+          tempName = '';
+        });
+        cb.onEndDocument(() => {
+          // 按收到的消息格式回复消息
+          CreateTime = parseInt(new Date().getTime() / 1000, 10);
+          let msg = '';
+          if (MsgType === 'text') {
+            msg = Content;
+            // 组织返回的数据包
+            const sendMessage = `
+                <xml>
+                  <ToUserName><![CDATA[${FromUserName}]]></ToUserName>
+                  <FromUserName><![CDATA[${ToUserName}]]></FromUserName>
+                  <CreateTime>${CreateTime}</CreateTime>
+                  <MsgType><![CDATA[text]]></MsgType>
+                  <Content><![CDATA[${msg}]]></Content>
+                </xml>`;
+            res.end(sendMessage);
+          }
+        });
+      });
+      parse.parseString(xmlStr);
       res.end(echostr);
     });
   } else {
