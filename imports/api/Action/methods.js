2dcf9238b43246174421779c7903ecd09bda7f49
周瑾 <zhoujin@zhoujins-MacBook-Air.local>
2017-06-16 02:48:34 +0800

    step0

--- imports/api/Action/methods.js
+++ imports/api/Action/methods.js
@@ -1,34 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
-import { Mongo } from 'meteor/mongo';
-const Future = require('fibers/future');
-import _ from 'lodash';
 
 Meteor.methods({
   // 获取某一个实体，用get
   'test'() {
     return 'hello world';
-  },
-  'reply'(text) {
-    check(text, String);
-    const replyFuture = new Future();
-    HTTP.post(
-      'http://www.tuling123.com/openapi/api',
-      {
-        params: {
-          key: '3384f71c080595dd5d1eae97fe5a66c3',
-          info: text,
-        },
-      },
-      (err, res) => {
-        let reply = '';
-        if (res && res.statusCode === 200) {
-          const content = JSON.parse(res.content);
-          reply = content.text;
-        }
-        replyFuture.return(reply);
-      }
-    );
-    return replyFuture.wait();
   },
 });
